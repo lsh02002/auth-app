ef3e028181c822f0dd18ce90efb24048d72d3ce1
lsh02002 <lsh02002@hanmail.net>
2024-02-07 00:34:15 +0900

    userId added

--- src/components/Login/Redirect.tsx
+++ src/components/Login/Redirect.tsx
@@ -29,9 +29,11 @@ const Redirect: React.FC<RedirectProps> = ({ updateIsToken }) => {
 
           const token = res.headers.token;
           const nickName = res.data.data.nickName;
+          const userId = res.data.data.userId;
 
           localStorage.setItem("token", token);
           localStorage.setItem("nickName", nickName);
+          localStorage.setItem("userId", userId);
 
           updateIsToken(true);
 
